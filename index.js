9fec6b88cb8c1fd85efb168e05ed778fe1767734
margaretta hemmings <mbcc1905@hotmail.com>
2018-04-26 23:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 var name = "Janet"
 var height = "74"
 
-console.log("Name:")
-console.log(name)
+//console.log("Name:")
+//console.log(name)
 
-console.log("Height:")
+/*console.log("Height:")
 console.log(height)
-console.log(parseInt(height) +1) 
+console.log(parseInt(height) +1)
+*/
+
+var info = "" + name + " is " + height " inches tall!";
